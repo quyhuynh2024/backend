b4aaac406a4f42bc3189bd1ea0722a8b07153f82
quyhuynh2024 <quyhuynh2024@gmail.com>
2024-03-22 23:13:56 +0700

    update log

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -11,15 +11,6 @@ export function errorHandler(
     statusCode: err.statusCode || 500,
     message: err.message || "Something went wrong, try again later",
   };
-  /* if (err.name === "ValidationError") {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST;
-    defaultError.msg = Object.values(err.errors)
-      .map((item) => item.message)
-      .join(",");
-  }
-  if (err.code && err.code === 11000) {
-    defaultError.statusCode = StatusCodes.BAD_REQUEST;
-    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
-  } */
+  
   res.status(defaultError.statusCode).json({ message: defaultError.message });
 }
--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export function logger(req: Request, res: Response, next: NextFunction) {
   res.on("finish", function () {
     console.log(
-      req.timestamp,
+      new Date(req.timestamp as number),
+      req.header("x-forwarded-for") || req.socket.remoteAddress,
       req.method,
       decodeURI(req.url),
       res.statusCode,
